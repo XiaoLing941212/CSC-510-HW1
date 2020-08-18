9ff605624681f789609358c976c02f57d6ee4f50
Xiao Ling <41660376+XiaoLing941212@users.noreply.github.com>
2020-08-17 21:37:14 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name="CSC-510-HW1",
+    version="0.0.1",
+    author="Xiao Ling",
+    author_email='xling4@ncsu.edu',
+    url='https://github.com/XiaoLing941212/CSC-510-HW1',
+    packages=['CSC-510-HW1'],
+      long_description="""\
+        Homework 1 for CSC 510
+      """,
+      classifiers=[
+          "License :: OSI Approved :: MIT",
+          "Programming Language :: Python",
+      ],
+      keywords='CSC-510 Homework 1',
+      license='MIT',
+      install_requires=[
+        'numpy',
+        'scipy',
+      ],
+      )
